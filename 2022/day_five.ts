af9ae291261d6109a0c656bd1b4aa0cc9669b08f
Patrick Brady <patrick.brady24@outlook.com>
2022-12-05 21:15:26 -0600

    2022 - Day 5 Part II

--- 2022/day_five.ts
+++ 2022/day_five.ts
@@ -1,3 +1,6 @@
+// What I learned: Using a collection decorator REALLY cleaned up this code
+// There was a decent amount of duplication and all of it was operating on Crates.
+
 function parse(input: string) {
   const lines = input.split("\n");
 
@@ -6,31 +9,45 @@ function parse(input: string) {
 
 class Instruction {
   static parse(input: string) {
-    const [_0, iterations, _1, from, _2, to] = input.split(" ");
+    const [_0, count, _1, from, _2, to] = input.split(" ");
 
-    return new Instruction(parseInt(iterations), parseInt(from), parseInt(to));
+    return new Instruction(parseInt(count), parseInt(from), parseInt(to));
   }
 
-  constructor(
-    public iterations: number,
-    public from: number,
-    public to: number
-  ) {}
+  constructor(public count: number, public from: number, public to: number) {}
+}
+export class Crates {
+  constructor(private crates: string[][]) {}
+
+  public get(stack: number, count: number) {
+    return this.crates[stack - 1].splice(-1 * count);
+  }
+
+  public move(stack: number, crates: string[]) {
+    this.crates[stack - 1].push(...crates);
+  }
+
+  public getTops() {
+    return this.crates.map((stack) => stack[stack.length - 1]).join("");
+  }
 }
 
-export function partOne(crates: string[][], input: string) {
+export function partOne(crates: Crates, input: string) {
   const instructions = parse(input);
 
-  instructions.forEach(({ iterations, from, to }) => {
-    for (let i = 0; i < iterations; i++) {
-      const crate = crates[from - 1].pop();
-      if (!crate) {
-        return;
-      }
-
-      crates[to - 1].push(crate);
-    }
+  instructions.forEach(({ count, from, to }) => {
+    crates.move(to, crates.get(from, count).reverse());
   });
 
-  return crates.map((crate) => crate.pop()).join("");
+  return crates.getTops();
+}
+
+export function partTwo(crates: Crates, input: string) {
+  const instructions = parse(input);
+
+  instructions.forEach(({ count, from, to }) => {
+    crates.move(to, crates.get(from, count));
+  });
+
+  return crates.getTops();
 }
